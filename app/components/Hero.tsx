873f4fa9f9163673fece9c0b43e376d924d05d7b
Popeyee <elhachimiali1@gmail.com>
2025-11-20 14:21:13 -0500

    impl- Hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useRef } from "react";
+
+const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) videoRef.current.playbackRate = 2;
+  }, []);
+  return (
+    <section id="hero">
+      <div>
+        <h1>MacBook Pro</h1>
+        <Image src="/title.png" alt="Macbook title" width={500} height={420} />
+      </div>
+
+      <video
+        src="/videos/hero.mp4"
+        ref={videoRef}
+        autoPlay
+        muted
+        playsInline
+      ></video>
+      <button>Buy</button>
+      <p>From $1599 or $133/mo for 12 months</p>
+    </section>
+  );
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <>
-      <div className="  "></div>
+      <div>
+        <Hero />
+      </div>
     </>
   );
 }
